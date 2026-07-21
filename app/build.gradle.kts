b27598889d202b6b2d1af170640a4a8c57f3eb23
mirad-tech <miradwhitehead@gmail.com>
2026-07-22 03:07:33 +0800

    release: prepare 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.example.skip"
         minSdk = 28
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.0.11"
+        versionCode = 20
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
